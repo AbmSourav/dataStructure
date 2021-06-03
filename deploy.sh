ae6ccda75d208140b5fafbb70230e6faa6d4abad
AbmSourav <keramotul.islam@gmail.com>
2021-06-03 17:17:37 +0600

    deploy bash script added

---
+++ deploy.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+echo "Press 'Y' to start deployment"
+read confirmation
+
+if [ $confirmation != 'Y' ]; then
+	echo "Exiting..."
+	exit;
+fi
+
+echo "Deleting hidden settings..."
+vscode=".vscode"
+if [ -d $vscode ]; then rm -R $vscode; fi
+
+echo "Release Version: "
+read version
+
+echo "Release comments: "
+read comments
+echo ""
+
+echo "Creating git tag..."
+# git tag $version -m "$comments"
+
+
+echo "Press Y to push new release"
+read pushNow
+if [ $pushNow == 'Y' ]; then
+	git push origin $version
+	exit;
+fi
