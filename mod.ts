b77a16c9ab8ef2db81dc63e48adb3bd3167ed4cf
AbmSourav <keramotul.islam@gmail.com>
2021-06-03 17:43:31 +0600

    comment doc added in mod file

--- mod.ts
+++ mod.ts
@@ -1,3 +1,19 @@
+/**
+ * Data Structure
+ * 
+ * Different Data Structures implementation API provided by this module.
+ * API available in TypeScript & JavaScript.
+ * 
+ * @package dataStructure
+ * @author Keramot UL Islam [Abm Sourav]
+ * @email <keramotul.islam@gmail.com>
+ * @authorUrl https://abmsourav.com
+ * @sourceCode https://github.com/AbmSourav/dataStructure
+ * @version 0.0.6
+ * 
+ * Copyright (c) 2021 Keramot UL Islam
+ */
+
 export { SinglyLinkedList } from "./linkedList/singly/singlyLinkedList.ts";
 export { DoublyLinkedList } from "./linkedList/doubly/doublyLinkedList.ts";
 export { Stack } from "./stack/stack.ts"
