56692a9b6ca126fb820b1e71d60c08f97efd83db
AbmSourav <keramotul.islam@gmail.com>
2021-06-21 17:45:29 +0600

    blockChain JS file added, mod files updated

--- mod.js
+++ mod.js
@@ -9,13 +9,14 @@
  * @email <keramotul.islam@gmail.com>
  * @authorUrl https://abmsourav.com
  * @sourceCode https://github.com/AbmSourav/dataStructure
- * @version 1.0.4
+ * @version 1.0.5
  * 
  * Copyright (c) 2021 Keramot UL Islam
  */
 
- export { HashTable } from "./hashTable/hashTable.bundle.js";
- export { SinglyLinkedList } from "./linkedList/singly/singlyLinkedList.bundle.js";
- export { DoublyLinkedList } from "./linkedList/doubly/doublyLinkedList.bundle.js";
- export { Stack } from "./stack/stack.bundle.js"
- export { Queue } from "./queue/queue.bundle.js"
+export { BlockChain } from "./blockChain/blockChain.bundle.js";
+export { HashTable } from "./hashTable/hashTable.bundle.js";
+export { SinglyLinkedList } from "./linkedList/singly/singlyLinkedList.bundle.js";
+export { DoublyLinkedList } from "./linkedList/doubly/doublyLinkedList.bundle.js";
+export { Stack } from "./stack/stack.bundle.js"
+export { Queue } from "./queue/queue.bundle.js"
