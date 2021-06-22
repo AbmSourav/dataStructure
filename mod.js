a1e59a31993f99f1a3dfdb23700db7924c6e826f
AbmSourav <keramotul.islam@gmail.com>
2021-06-22 13:15:52 +0600

    version updated to 1.1.0

--- mod.js
+++ mod.js
@@ -9,7 +9,7 @@
  * @email <keramotul.islam@gmail.com>
  * @authorUrl https://abmsourav.com
  * @sourceCode https://github.com/AbmSourav/dataStructure
- * @version 2.0.0
+ * @version 1.1.0
  * 
  * Copyright (c) 2021 Keramot UL Islam
  */
